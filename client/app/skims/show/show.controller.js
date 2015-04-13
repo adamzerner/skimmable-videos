a5a07f1e5f2276208daa58ce0a636959c2b1fea9
Adam Zerner <azerner@GNVazernerVCG3QC.local>
2015-04-13 00:23:05 -0400

    Section durations shown. First iteration of show page done.

--- client/app/skims/show/show.controller.js
+++ client/app/skims/show/show.controller.js
@@ -16,9 +16,10 @@ function ShowCtrl (Skim, $stateParams) {
         var duration = '(';
         if (vm.skim.hours > 0) duration += vm.skim.hours + ' hours, ';
         duration += vm.skim.minutes + ' minutes, ';
-        duration += vm.skim.seconds + ' seconds)'
+        duration += vm.skim.seconds + ' seconds)';
         return duration;
       };
+      getSkimDurations();      
     });
 
   vm.play = function(player, subsection) {
@@ -26,6 +27,35 @@ function ShowCtrl (Skim, $stateParams) {
     player.seekTo(seconds);
     player.playVideo();
   };
+
+  function getSkimDurations() {
+    var curr, next, currStart, nextStart, hours, minutes, seconds, noSubsections;
+    debugger;
+    for (var i = 0, len = vm.skim.sections.length; i < len; i++) {
+      curr = vm.skim.sections[i];
+      next = vm.skim.sections[i+1] 
+                  || curr.subsections[curr.subsections.length-1]; // last section
+      if (!next) {
+        next = vm.skim; // last section AND no subsections
+        noSubsections = true;
+      }
+      currStart = curr.hour*60*60 + curr.minute*60 + curr.second;
+      if (noSubsections) nextStart = next.hours*60*60 + next.minutes*60 + next.seconds;
+      else nextStart = next.hour*60*60 + next.minute*60 + next.second;
+      seconds = nextStart - currStart;
+      hours = Math.floor(seconds/60/60);
+      seconds -= hours*60*60;
+      minutes = Math.floor(seconds/60);
+      seconds -= minutes*60;
+      curr.duration = '(';
+      if (hours >= 2) curr.duration += hours + ' hours, ';
+      else if (hours === 1) curr.duration += hours + 'hour, ';
+      if (minutes >= 2) curr.duration += minutes + ' minutes, ';
+      else if (minutes === 1) curr.duration += minutes + ' minute, ';
+      if (seconds >= 2) curr.duration += Math.round(seconds) + ' seconds)';
+      else curr.duration += Math.round(seconds) + ' second)';
+    }
+  }
 }
 
 

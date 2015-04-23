91d586f60732eacbe38bf0cf9c9152bd025d7ed5
Adam Zerner <azerner@GNVazernerVCG3QC.local>
2015-04-22 22:26:58 -0400

    Fixed navbar username data binding bug

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -9,10 +9,14 @@ angular.module('skimmableVideosApp')
 function SettingsCtrl(Auth, User, $state, $rootScope) {
   var vm = this;
   vm.currUser = Auth.getCurrentUser();
+  vm.name = vm.currUser.name;
+  vm.email = vm.currUser.email;
   vm.update = function() {
+    vm.currUser.name = vm.name; // because annoying data binding problem with navbar
+    vm.currUser.email = vm.email;
     User.save(vm.currUser, function(user) {
       $rootScope.$emit('refresh_navbar');
-      $state.go('user', {id: user._id}, {reload: true});
+      $state.go('profile', {id: user._id}, {reload: true});
     });
   };
 }
